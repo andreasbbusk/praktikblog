1c65bc70a3de6d9ac0d74b6d828dcc20c8ecd82e
andreasbbusk <144665042+andreasbbusk@users.noreply.github.com>
2025-01-18 13:20:49 +0100

    Addition of grid background

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,12 @@ export default function RootLayout({
         <SessionProvider>
           <AuthProvider>
             <Navbar />
-            <div className="container mx-auto py-8">{children}</div>
+            <div className="h-[50rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative">
+              <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+              <div className="container mx-auto py-8 relative z-10">
+                {children}
+              </div>
+            </div>
           </AuthProvider>
         </SessionProvider>
       </body>
